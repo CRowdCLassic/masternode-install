65b501ad10c42067307dcae13f540246ef13fdcb
cryptocrowdclub <44811053+cryptocrowdclub@users.noreply.github.com>
2018-12-05 21:11:39 +0200

    Corrected for cold wallet

--- cold_mn_install.sh
+++ cold_mn_install.sh
@@ -261,13 +261,13 @@ spin='-\|/'
 # sleep 60
 # ./crowdclassicd -daemon
 sleep 10
-masternodeStartOutput=$(./crowdclassic-cli masternode start)
+masternodeStartOutput=$(./crowdclassic-cli masternode status)
 echo $masternodeStartOutput
 while [[ ! ($masternodeStartOutput = *"started"*) ]]; do
         i=$(( (i+1) %4 ))
         block=`./crowdclassic-cli getinfo | grep block | tr -d ,`
         balance=`./crowdclassic-cli getbalance`
-        masternodeStartOutput=$(./crowdclassic-cli masternode start)
+        masternodeStartOutput=$(./crowdclassic-cli masternode status)
         printf "\r$block | Balance : $balance ${spin:$i:1} : $masternodeStartOutput                "
         sleep 5
 done
@@ -275,8 +275,8 @@ echo ""
 echo "Add sentinelLinux in crontab"
 (crontab -l 2>/dev/null; echo "* * * * * cd ~/sentinelLinux && ./venv/bin/python bin/sentinel.py 2>&1 >> sentinel-cron.log") | crontab -
 echo ""
-echo "Add check MN Status in crontab"
-(crontab -l 2>/dev/null; echo "* * * * * cd ~/masternode-install &  bash check_status.sh 2>&1 >> mn-check-cron.log") | crontab -
+# echo "Add check MN Status in crontab"
+# (crontab -l 2>/dev/null; echo "* * * * * cd ~/masternode-install &  bash check_status.sh 2>&1 >> mn-check-cron.log") | crontab -
 sudo service cron reload
 echo "$masternodeStartOutput"
 sudo apt-get autoremove -y
